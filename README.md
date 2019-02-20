3e8fea57ced95eb6cb00097612525d603d222135
Lucian Maly <lucky@senior.cz>
2019-02-20 22:44:45 +1100

    Still WIP, can print struct now

--- README.md
+++ README.md
@@ -1,4 +1,84 @@
 # DZone: Programming & DevOps Refcardz Downloader
 
-- URL: https://dzone.com/refcardz
-- Amount: 286 refcardz (2019-02-17)
+## What is DZone.com
+
+- DZone.com is one of the world's largest online communities and leading publisher of knowledge resources for software developers. Every day, hundreds of thousands of developers come to DZone.com to read about the latest technology trends and learn about new technologies, methodologies, and best practices through shared knowledge.
+
+## Requirements
+
+- Golang `1.10.2` or higher.
+- [DZone](https://dzone.com) free account.
+
+## Application
+
+- Run `go run main.go`.
+
+## Technical details of the solution
+
+1. Loop through the assets list websites - `https://dzone.com/services/widget/assets-listV2/DEFAULT?hidefeat=true&page=XX&sort=downloads&type=refcard`, where `XX` is `1` to `XX` (until this empty response is returned: `{"success":true,"result":{"data":{"assets":[],"sort":"downloads"}},"status":200}`). At the time of writing, there are `24` pages. See this example of the valid JSON response:
+
+```json
+{
+  "success": true,
+  "result": {
+    "data": {
+      "assets": [
+        <---------- OMITTED ---------->
+        {
+          "id": 520107,
+          "title": "GWT Style, Configuration and JSNI Reference",
+          "details": "Introduces Ajax, a group interrelated techniques used in client-side web development for creating asynchronous web applications.",
+          "subtitle": "Using the Google Web Toolkit",
+          "collaborators": "Jill Tomich",
+          "downloads": 29488,
+          "views": 115116,
+          "cover": "//dz2cdn3.dzone.com/storage/rc-covers/2806-dzone_refcard_.png",
+          "host": null,
+          "url": "/refcardz/gwt-style-configuration-and-js",
+          "tags": [
+            "frameworks",
+            "javascript",
+            "server-side",
+            "java",
+            "web dev",
+            "ajax &amp; scripting"
+          ],
+          "color": "purple",
+          "type": "refcard",
+          "pdf": "/asset/download/6",
+          "authors": [
+            {
+              "id": 327457,
+              "name": "Robert Hansen",
+              "avatar": "https://secure.gravatar.com/avatar/ae431e508cbc54620c27a0d612d4f93c?d=identicon&r=PG",
+              "url": "/users/327457/rhansen1392.html"
+            }
+          ],
+          "saveStatus": {
+            "saved": false,
+            "canSave": true,
+            "count": 63
+          }
+        }
+        <---------- OMITTED ---------->
+      ],
+      "sort": "downloads"
+    }
+  },
+  "status": 200
+}
+```
+
+2. On each of these asset list websites, extract the following information from the returned JSON file:
+
+    - Title:      `'.result.data.assets[].title'`.
+    - PDF suffix: `'.result.data.assets[].pdf'`.
+
+3. Download the PDF by prefixing the PDF link with `https://dzone.com`, creating e.g. `https://dzone.com/asset/download/279342` and save it as `<Title>.pdf`, e.g. `GWT_Style,_Configuration_and_JSNI_Reference.pdf`
+
+## Stats
+
+|Item          |Size      |
+|--------------|----------|
+|Refcardz      |284       |
+|All files size|000 B (GB)|
--- main.go
+++ main.go
@@ -1,50 +1,98 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"os"
+	"strconv"
 
 	"github.com/gocolly/colly"
 )
 
-// variables
-var (
-	username = "XXX" // !!! change to your e-mail address
-	password = "XXX" // !!! enter your password here
+// constants that do not change
+const (
+	username = "dzone-refcardz@mailcatch.com" // created just for this purpose
+	password = "password123456"               // created just for this purpose
 )
 
-func main() {
-	// instantiate default collector
-	c := colly.NewCollector()
+func main() { // main function start
 
-	// authenticate
-	err := c.Post("https://dzone.com/services/internal/action/users-login", map[string]string{"username": username, "password": password})
-	if err != nil {
-		log.Fatal(err)
-	}
+	// instantiate first collector
+	f := colly.NewCollector()
 
-	// attach callbacks after login
-	c.OnResponse(func(r *colly.Response) {
-		log.Println("response received", r.StatusCode)
-	})
+	// always start with page zero
+	page := 0
 
-	//
-	var counter = 0
-	c.OnHTML("div[class=asset-subtitle] a[href]", func(f *colly.HTMLElement) {
-		title := f.Text
-		fmt.Println("Book title is:", title)
-		counter++
-	})
+	// construct the JSON structure
+	data := struct {
+		Result struct {
+			Data struct {
+				Assets []struct {
+					Title string `json:"title"`
+					Pdf   string `json:"pdf"`
+				}
+			}
+		}
+	}{}
 
-	fmt.Println("Amount of Refcardz found:", counter)
+	// keep visiting the assets-list websites until the specific response indicating last page is returned
+	for {
 
-	c.OnHTML("dz-download", func(d *colly.HTMLElement) {
-		download := d.Attr("asset")
-		// Download link example: https://dzone.com/asset/download/279342
-		fmt.Println("Download link: " + "https://dzone.com" + download)
-	})
+		// is there any error in the response?
+		f.OnError(func(r *colly.Response, e error) {
+			log.Println("Error:", e, r.Request.URL, string(r.Body))
+		})
 
-	// start scraping Refcardz
-	c.Visit("https://dzone.com/refcardz")
+		// perform the following block on each response
+		f.OnResponse(func(r *colly.Response) {
 
-} // end of func main()
+			// when the response is the last page, stop continuing with other pages
+			if string(r.Body) == `{"success":true,"result":{"data":{"assets":[],"sort":"downloads"}},"status":200}` {
+				fmt.Println("Last page reached")
+				defer fmt.Println("!")
+				os.Exit(0)
+			}
+
+			// unmarshal JSON based on the struct
+			err := json.Unmarshal([]byte(r.Body), &data)
+			if err != nil { // eventually error out
+				log.Fatal(err)
+			}
+
+			// naked return; returns the current values in the return arguments local variables
+			return
+		}) // end of response block
+
+		fmt.Println("Struct #", page, ":") // display the current page
+		fmt.Println(data)                  // only for testing, display the struct of the current page
+
+		// TODO: Perform something like below for each struct data
+		// instantiate new collector
+		// p := colly.NewCollector()
+
+		// authenticate, otherwise you won't be able to download refcardz
+		// err := p.Post("https://dzone.com/services/internal/action/users-login", map[string]string{"username": username, "password": password})
+		// if err != nil {
+		//	log.Fatal(err)
+		// }
+
+		// attach callbacks after login
+		// p.OnResponse(func(q *colly.Response) {
+		//	log.Println("Login response received:", q.StatusCode)
+		//	replace the " " with a "_" in the filename
+		//	result := strings.Replace(title, " ", "_", -1)
+		//	q.Save(result)
+		// })
+
+		// download link example: https://dzone.com/asset/download/279342
+		// p.Visit("https://dzone.com" + pdf)
+
+		page++ // increase counter after visiting the assets-list webpage
+
+		// assets-list website construction
+		f.Visit("https://dzone.com/services/widget/assets-listV2/DEFAULT?hidefeat=true&page=" + strconv.Itoa(page) + "&sort=downloads&type=refcard")
+
+	} // end of the for loop
+
+} // end of the main function
