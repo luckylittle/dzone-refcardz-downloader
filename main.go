b0f9dfdcd4a9f80fd254d9c1396997c45aac07d8
Lucian Maly <lucky@senior.cz>
2019-02-22 00:26:07 +1100

    Almost finished, but log in does not work yet

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/gocolly/colly"
 )
@@ -16,43 +17,55 @@ const (
 	password = "password123456"               // created just for this purpose
 )
 
+// RefcardzData is an exported title and pdf link
+type RefcardzData struct {
+	Result struct {
+		Data struct {
+			Assets []struct {
+				Title string //`json:"title"`
+				Pdf   string //`json:"pdf"`
+			} `json:"assets"`
+		} `json:"data"`
+	} `json:"result"`
+}
+
+// global variables
+var data RefcardzData
+
 func main() { // main function start
 
-	// instantiate first collector
+	// create folder for downloads
+	os.Mkdir("downloads", 0700)
+
+	// instantiate first collector for log in
 	f := colly.NewCollector()
 
-	// always start with page zero
-	page := 0
+	// instantiate second collector for assets-list
+	g := colly.NewCollector()
 
-	// construct the JSON structure
-	data := struct {
-		Result struct {
-			Data struct {
-				Assets []struct {
-					Title string `json:"title"`
-					Pdf   string `json:"pdf"`
-				}
-			}
-		}
-	}{}
+	// null the counters/pagers
+	page := 0
+	counter := 1
+
+	// authenticate, otherwise you won't be able to download refcardz later
+	err := f.Post("https://dzone.com/services/internal/action/dzoneUsers-validateCredentials", map[string]string{"username": username, "password": password})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// visit the users-login page
+	f.Visit("https://dzone.com/services/internal/action/users-login")
 
 	// keep visiting the assets-list websites until the specific response indicating last page is returned
 	for {
 
 		// is there any error in the response?
-		f.OnError(func(r *colly.Response, e error) {
+		g.OnError(func(r *colly.Response, e error) {
 			log.Println("Error:", e, r.Request.URL, string(r.Body))
 		})
 
-		// perform the following block on each response
-		f.OnResponse(func(r *colly.Response) {
-
-			// when the response is the last page, stop continuing with other pages
-			if string(r.Body) == `{"success":true,"result":{"data":{"assets":[],"sort":"downloads"}},"status":200}` {
-				fmt.Println("Last page reached")
-				defer fmt.Println("!")
-				os.Exit(0)
-			}
+		// perform the following block on each assets-page response
+		g.OnResponse(func(r *colly.Response) {
 
 			// unmarshal JSON based on the struct
 			err := json.Unmarshal([]byte(r.Body), &data)
@@ -60,38 +73,42 @@ func main() { // main function start
 				log.Fatal(err)
 			}
 
+			// when the response is the last page, stop continuing with other pages and exit
+			if string(r.Body) == `{"success":true,"result":{"data":{"assets":[],"sort":"downloads"}},"status":200}` {
+				fmt.Println("Last page reached")
+				defer fmt.Println("!")
+				os.Exit(0)
+			}
+
 			// naked return; returns the current values in the return arguments local variables
 			return
+
 		}) // end of response block
 
-		fmt.Println("Struct #", page, ":") // display the current page
-		fmt.Println(data)                  // only for testing, display the struct of the current page
+		// clone the logging collector
+		h := f.Clone()
 
-		// TODO: Perform something like below for each struct data
-		// instantiate new collector
-		// p := colly.NewCollector()
+		// loop through the data assets containing Titles and incomplete Pdf links
+		for _, obj := range data.Result.Data.Assets {
+			filename := obj.Title                                            // title is going to be filename
+			link := "https://dzone.com" + obj.Pdf                            // complete the HTTP link
+			fmt.Println("#", counter, "Downloading", filename, "from", link) // show verbose progress
 
-		// authenticate, otherwise you won't be able to download refcardz
-		// err := p.Post("https://dzone.com/services/internal/action/users-login", map[string]string{"username": username, "password": password})
-		// if err != nil {
-		//	log.Fatal(err)
-		// }
+			h.OnResponse(func(q *colly.Response) {
+				// replace the " " with a "_" in the filename and add extension
+				result := strings.Replace("downloads/"+filename+".pdf", " ", "_", -1)
+				q.Save(result) // save
+			})
+			// visit
+			h.Visit(link)
+			counter++
+		}
 
-		// attach callbacks after login
-		// p.OnResponse(func(q *colly.Response) {
-		//	log.Println("Login response received:", q.StatusCode)
-		//	replace the " " with a "_" in the filename
-		//	result := strings.Replace(title, " ", "_", -1)
-		//	q.Save(result)
-		// })
+		// next page
+		page++
 
-		// download link example: https://dzone.com/asset/download/279342
-		// p.Visit("https://dzone.com" + pdf)
-
-		page++ // increase counter after visiting the assets-list webpage
-
-		// assets-list website construction
-		f.Visit("https://dzone.com/services/widget/assets-listV2/DEFAULT?hidefeat=true&page=" + strconv.Itoa(page) + "&sort=downloads&type=refcard")
+		// visitassets-list website construction
+		g.Visit("https://dzone.com/services/widget/assets-listV2/DEFAULT?hidefeat=true&page=" + strconv.Itoa(page) + "&sort=downloads&type=refcard")
 
 	} // end of the for loop
 
